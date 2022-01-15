5fc730c98c8d26fb62c121d9b09ba79f85796495
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-15 17:46:50 +0200

    feat(navbar): creates the navbar that will handle the auth actions, as well as its routes and better structure folders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import Main from './Main';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Main />
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
