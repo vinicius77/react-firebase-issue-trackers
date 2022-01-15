392d96a7e3fa3cd44b0daddad6a0ae711f2b7afa
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-15 14:16:16 +0200

    feat(crud-improvements): removes unnecessary (redundant) logic

--- src/Main.jsx
+++ src/Main.jsx
@@ -17,16 +17,11 @@ const Main = () => {
 	const issuesRef = collection(db, ISSUES);
 	const [issue, setIssue] = useState(initialState);
 	const [issuesList, setIssuesList] = useState([]);
-	const [loading, setloading] = useState(false);
 
 	useEffect(() => {
 		const unsub = onSnapshot(issuesRef, (snapshot) => {
-			let updatedList = [];
-			snapshot.docs.forEach((doc) => updatedList.push({ id: doc.id, ...doc.data() }));
-
-			setIssuesList(updatedList);
+			setIssuesList(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
 		});
-		setloading(false);
 
 		// unsubscribe from changes when unmounting
 		return () => unsub();
@@ -45,11 +40,8 @@ const Main = () => {
 		if ((issue && !issue.description) || !issue) return;
 
 		try {
-			const { id } = await addDoc(issuesRef, issue);
-			setloading(true);
-			setIssuesList([...issuesList, { id, ...issue }]);
+			await addDoc(issuesRef, issue);
 			setIssue(initialState);
-			setloading(false);
 		} catch (error) {
 			console.log(error);
 		}
@@ -63,10 +55,6 @@ const Main = () => {
 
 		try {
 			await deleteDoc(issueDocRef);
-			setloading(true);
-			const updatedList = issuesList.filter((issue) => issue.id !== e.target.id);
-			setIssuesList(updatedList);
-			setloading(false);
 		} catch (error) {
 			console.log(error);
 		}
@@ -80,11 +68,6 @@ const Main = () => {
 
 		try {
 			await updateDoc(issueRef, updatedIssue);
-			setloading(true);
-			const updatedList = issuesList.map((el) => (el.id === updatedIssue.id ? updatedIssue : el));
-
-			setIssuesList(updatedList);
-			setloading(false);
 		} catch (error) {
 			console.log(error);
 		}
@@ -95,92 +78,87 @@ const Main = () => {
 			<h2 className="section-subheading">Firebase Firestore Course</h2>
 			<h3 className="section-subheading">Issues List</h3>
 			<div className="results-div">
-				{!loading ? (
-					<table>
-						<thead className="highlight-cell">
-							<tr>
-								<th width="15%">Severity</th>
-								<th width="60%">Description</th>
-								<th width="15%">Resolved?</th>
-								<th width="10%">🦀</th>
-							</tr>
-						</thead>
+				<table>
+					<thead className="highlight-cell">
+						<tr>
+							<th width="15%">Severity</th>
+							<th width="60%">Description</th>
+							<th width="15%">Resolved?</th>
+							<th width="10%">🦀</th>
+						</tr>
+					</thead>
 
-						{issuesList &&
-							!loading &&
-							issuesList.map((issue) => {
-								return (
-									<tbody id="list-table-body" key={issue.id}>
-										<tr>
-											<td>{issue.severity}</td>
-											<td>{issue.description}</td>
-											<td>
-												<select
-													id="resolved"
-													value={issue.resolved}
-													onChange={(e) => onUpdatedResolved(e, issue)}
-												>
-													<option value="yes">yes</option>
-													<option value="no">no</option>
-												</select>
-											</td>
-											<td>
-												<button
-													className="button btn btn-danger"
-													readOnly
-													id={issue.id}
-													onClick={(e) => onDeleteHandler(e)}
-												>
-													X
-												</button>
-											</td>
-										</tr>
-									</tbody>
-								);
-							})}
+					{issuesList &&
+						issuesList.map((issue) => {
+							return (
+								<tbody id="list-table-body" key={issue.id}>
+									<tr>
+										<td>{issue.severity}</td>
+										<td>{issue.description}</td>
+										<td>
+											<select
+												id="resolved"
+												value={issue.resolved}
+												onChange={(e) => onUpdatedResolved(e, issue)}
+											>
+												<option value="yes">yes</option>
+												<option value="no">no</option>
+											</select>
+										</td>
+										<td>
+											<button
+												className="button btn btn-danger"
+												readOnly
+												id={issue.id}
+												onClick={(e) => onDeleteHandler(e)}
+											>
+												X
+											</button>
+										</td>
+									</tr>
+								</tbody>
+							);
+						})}
 
-						<tfoot className="empty-cell">
-							<tr>
-								<td>
-									<select id="severity" onChange={onChangeHandler}>
-										<option value="minor">minor</option>
-										<option value="moderate">moderate</option>
-										<option value="major">major</option>
-									</select>
-								</td>
-								<td>
-									<input
-										value={issue.description}
-										className="textfield"
-										id="description"
-										type="text"
-										name="description"
-										placeholder="e.g. your issue"
-										onChange={onChangeHandler}
-									/>
-								</td>
-								<td>
-									<select id="resolved" onChange={onChangeHandler}>
-										<option value="no">no</option>
-										<option value="yes">yes</option>
-									</select>
-								</td>
-								<td>
-									<button
-										className="button btn btn-primary"
-										readOnly
-										id="addButton"
-										onClick={(e) => onSubmitHandler(e)}
-									>
-										+
-									</button>
-								</td>
-							</tr>
-						</tfoot>
-					</table>
-				) : (
-					<p>Loading, please wait</p>
-				)}
+					<tfoot className="empty-cell">
+						<tr>
+							<td>
+								<select id="severity" onChange={onChangeHandler}>
+									<option value="minor">minor</option>
+									<option value="moderate">moderate</option>
+									<option value="major">major</option>
+								</select>
+							</td>
+							<td>
+								<input
+									value={issue.description}
+									className="textfield"
+									id="description"
+									type="text"
+									name="description"
+									placeholder="e.g. your issue"
+									onChange={onChangeHandler}
+								/>
+							</td>
+							<td>
+								<select id="resolved" onChange={onChangeHandler}>
+									<option value="no">no</option>
+									<option value="yes">yes</option>
+								</select>
+							</td>
+							<td>
+								<button
+									className="button btn btn-primary"
+									readOnly
+									id="addButton"
+									onClick={(e) => onSubmitHandler(e)}
+								>
+									+
+								</button>
+							</td>
+						</tr>
+					</tfoot>
+				</table>
 			</div>
 		</div>
 	);
