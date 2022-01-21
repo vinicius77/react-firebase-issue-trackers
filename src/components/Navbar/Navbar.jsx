996c14744ed734c183b900022a4dcb2d77ed327e
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-21 23:15:53 +0200

    feat(logout): implements the log out action

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,50 @@
-import { Link } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { Link, useNavigate } from 'react-router-dom';
+import { USERS } from '../../constants/users';
+import { auth, db } from '../../firebase/firebase-config';
 import './navbar.css';
 
 const Navbar = () => {
+	const navigate = useNavigate();
+	const onSignOutHandle = async () => {
+		try {
+			const userRef = doc(db, USERS, auth.currentUser.uid);
+			const updatedUser = {
+				isOnline: false,
+			};
+
+			// updates the user also on users collection
+			await updateDoc(userRef, updatedUser);
+
+			// Signs Out
+			await signOut(auth);
+
+			navigate('/');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<nav>
 			<h3>
 				<Link to="/">Home</Link>
 			</h3>
 			<div>
-				<Link to="/register">Register</Link>
-				<Link to="/login">Login</Link>
+				{auth.currentUser ? (
+					<>
+						<Link to="/profile">Profile</Link>
+						<button className="logout-btn" onClick={onSignOutHandle}>
+							Log out
+						</button>
+					</>
+				) : (
+					<>
+						<Link to="/register">Register</Link>
+						<Link to="/login">Login</Link>
+					</>
+				)}
 			</div>
 		</nav>
 	);
