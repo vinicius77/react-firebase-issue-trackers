01c5c21b3f8f01d0f11b51965be22fcffca319c8
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-22 19:25:13 +0200

    feat(folder-structure): replaces Issues page in its pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Issues from './components/Issues/Issues';
+import Issues from './pages/issues/Issues';
 import Navbar from './components/Navbar/Navbar';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import AuthProvider from './context/auth';
